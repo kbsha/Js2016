dd077aa6b3f5f147319642159ef03783a2f159f8
kibremoges fenta <kibremogesf@gmail.com>
2024-02-11 15:19:50 +0300

    todo list

--- test.js
+++ test.js
@@ -1,1 +1,21 @@
 alert('its just a test');
+display();
+function display(){
+    let entrance=prompt();
+    let a=entrance;
+    switch(a){
+        case a :
+           // alert('a'); 
+            console.log('a');
+        break;
+        case b :
+           // alert('b');
+            console.log('b'); 
+        break;
+        case c :
+            //alert('c');
+            console.log('c'); 
+        break;
+
+    }
+}
